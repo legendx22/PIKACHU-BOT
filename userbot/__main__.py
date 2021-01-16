32840834a0e978fe53fb0ee3f19bad84f2276830
Dark Marshy <73180137+srinivasop@users.noreply.github.com>
2021-01-16 14:30:12 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -60,7 +60,7 @@ if LOAD_ASSISTANT == True:
 else:
     print("Assitant is Not Loading As U Have Disabled")
 
-print("DARK COBRA userbot AND YOUR ASSISTANT is Active Enjoy Join @DarkCobra_Support For Updates.")
+print("DARK COBRA userbot AND YOUR ASSISTANT is Active Enjoy Join @King_bot_officialchat for Updates.")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
